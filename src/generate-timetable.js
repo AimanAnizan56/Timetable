7a58e8fae7cc7708fbe1a9649c9d3e80782c2c0f
Aiman Anizan <anizanaimann1@gmail.com>
2021-05-05 17:07:00 +0800

    Initial Commit

---
+++ src/generate-timetable.js
@@ -0,0 +1,46 @@
+fetch("src/timetable.json")
+    .then((response) => response.json())
+    .then((data) => {
+        extract(data);
+    });
+function extract(data) {
+    // console.log(data.Day.length);
+    const Timetable = data.Timetable;
+    for (let index = 0; index < Timetable.length; index++) {
+        const Day = Timetable[index].Day;
+        const Subject = Timetable[index].Subject;
+
+        console.log(Day);
+        for (let index = 0; index < Subject.Name.length; index++) {
+            const SubjectName = Subject.Name[index];
+            const Lecturer = Subject.Lecturer[index];
+            const Time = Subject.Time[index];
+
+            generate_timetable(SubjectName, Lecturer, Time);
+            // console.log("\t" + SubjectName + ": " + Time + " || " + Lecturer);
+        }
+    }
+}
+
+function generate_timetable(subject, lecturer, time) {
+    var span_day;
+}
+
+/* 
+<div
+    class="bg-gradient-to-r from-pink-600 to-blue-600 p-2 my-3 rounded-md shadow-inner"
+>
+    <!-- Name of the day -->
+    <span class="font-medium px-2">Monday</span>
+    <div>
+        <!-- Name of subject and time -->
+        <div class="bg-white text-black p-2 my-2 rounded-md bg-opacity-50 text-xl">
+            <h3 class="font-bold">ICT500</h3>
+            <p class="pl-4 oldstyle-nums font-medium">10.00 am ~ 11.50 am</p>
+            <p class="mt-3 pr-2 text-sm font-medium text-right underline">
+                Sir Muhammad Hamiz Mohd Radzi
+            </p>
+        </div>
+    </div>
+</div>
+*/
